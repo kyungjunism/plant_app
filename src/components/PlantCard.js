8b79d9eef000332d6674b41cdec82549cc1747bf
KJ Park <kyupark1@publicisgroupe.net>
2020-07-18 15:38:25 -0400

    updated card and form

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -3,14 +3,20 @@ import { Card, Image } from 'semantic-ui-react';
 
 class PlantCard extends React.Component {
 	render() {
-		const { plant } = this.props;
+		const { plants } = this.props;
 		return (
 			<Card>
 				<Card.Content>
 					<Image floated="right" size="mini" src="https://react.semantic-ui.com/images/avatar/large/steve.jpg" />
 					<Card.Header>
 						<span>
-							<Card.Meta>{plant.last_watered_date}</Card.Meta>
+							<Card.Meta>Status: {plants.plant_name}</Card.Meta>
+							<Card.Description>
+								<p>Watering Duration: {plants.watering_duration}</p>
+								<p>Water Overdue: {plants.water_overdue}</p>
+								<p>Repotted Date: {plants.repotted_date}</p>
+								<p>Ideal Temperature: {plants.ideal_temperature}</p>
+							</Card.Description>
 						</span>
 					</Card.Header>
 				</Card.Content>
--- src/containers/PlantContainer.js
+++ src/containers/PlantContainer.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import PlantCard from '../components/PlantCard';
+import { Card } from 'semantic-ui-react';
 
 class PlantContainer extends React.Component {
-	renderPlants() {
-		const { plants } = this.props;
-		return plants.map((plant) => {
-			return (
-				<div>
-					<p>{plant.plant_name}</p>
-					<p>{plant.watering_duration}</p>
-					<p>{plant.last_watered_date}</p>
-					<p>{plant.water_overdue}</p>
-					<p>{plant.ideal_temperature}</p>
-				</div>
-			);
-		});
-	}
-
 	render() {
-		return <PlantCard>this.renderPlants()</PlantCard>;
+		const { plants } = this.props;
+
+		let renderPlants = [...plants];
+
+		return (
+			<Card.Group>
+				{renderPlants.map((plants) => {
+					return <PlantCard plants={plants} />;
+				})}
+			</Card.Group>
+		);
 	}
 }
 
